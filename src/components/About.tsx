898b03bce33f87bdc086580dcb9a54f23203c742
Fabrice <mwanafunzifabrice@gmail.com>
2023-07-08 00:31:07 +0200

    pushing tramslations

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,7 +1,6 @@
 // About.tsx
 import { useEffect, useState } from "react";
-import "tailwindcss/tailwind.css";
-
+import { useTranslation } from "react-i18next";
 interface AnimatedNumberProps {
   value: number;
 }
@@ -26,6 +25,7 @@ const AnimatedNumber: React.FC<AnimatedNumberProps> = ({ value }) => {
 };
 
 const About = () => {
+  const { t } = useTranslation();
   
   return (
     <section id="about" className="bg-yellow-700 h-fit min-h-screen  flex flex-col justify-center items-center text-center relative"> {/* Added relative positioning */}
@@ -33,34 +33,34 @@ const About = () => {
         <div className="flex justify-center flex-col laptop:space-y-10 space-y-5 laptop:w-[50%] w-[90%] tablet:w-[70%] mx-auto">
           <div className="flex justify-center flex-col laptop:space-y-5 space-x-3">
             <div className="flex justify-center">
-              <h1 className="laptop:text-2xl text-md font-bold text-black mx-0 animate-fade-in  animate-bounce">HELLO THERE</h1>
+              <h1 className="laptop:text-2xl text-md font-bold text-black mx-0 animate-fade-in  animate-bounce">{t('hello_there')}</h1>
             </div>
             <div className="flex justify-center">
-              <h1 className="laptop:text-5xl tablet:text-3xl text-2xl font-bold text-white mx-0 animate-fade-in animate-bounce underline-offset-6">WE ARE CREATIVE POETA</h1>
+              <h1 className="laptop:text-5xl tablet:text-3xl text-2xl font-bold text-white mx-0 animate-fade-in animate-bounce underline-offset-6">{t('we_are_creativa_poeta')}</h1>
             </div>
           </div>
           <div className="flex justify-center mx-auto h-px bg-white animate-bounce w-full">
           </div>
         </div>
         <div className="flex justify-center text-center laptop:px-40 px-4">
-          <h1 className="laptop:text-2xl leading-relaxed	 tablet:text-2xl text-center text-white mx-0 animate-fade-in animate-bounce underline-offset-6">Passionate about artistic expression and inspiring creation, we are a company of experienced designers, copywriters and web developers who offer you unique creative solutions. Our primary goal is to understand your needs in order to create exceptional results. With our approach of creativity and excellence, we capture attention and leave a lasting impression. Please contact us today to discuss your projects and find out how we can help you achieve your creative goals.</h1>
+          <h1 className="laptop:text-2xl leading-relaxed	 tablet:text-2xl text-center text-white mx-0 animate-fade-in animate-bounce underline-offset-6">{t('about_us_content')}</h1>
         </div>
         <div className="grid animate-bounce p-4 gap-4 laptop:px-[6%] laptop:grid-cols-4 tablet:grid-cols-2 px-20 grid-cols-1">
         <div className="flex flex-col space-y-8 p-4 justify-center laptop:border-r-2 border-b-2 laptop:border-b-0 px-5">
           <AnimatedNumber value={127} />
-          <p className="text-black font-bold text-2xl">Awards received</p>
+          <p className="text-black font-bold text-2xl">{t('awards_received')}</p>
         </div>
         <div className="flex flex-col space-y-8 p-4 justify-center laptop:border-r-2 laptop:border-b-0 border-b-2 px-5">
           <AnimatedNumber value={1202} />
-          <p className="text-black font-bold text-2xl">Cups Of Coffee</p>
+          <p className="text-black font-bold text-2xl">{t('cups_of_coffee')}</p>
         </div>
         <div className="flex flex-col space-y-8 p-4 justify-center laptop:border-r-2 border-b-2 tablet:border-b-0 border-r-0 px-5">
           <AnimatedNumber value={201} />
-          <p className="text-black font-bold text-2xl">Happy Clients</p>
+          <p className="text-black font-bold text-2xl">{t('happy_clients')}</p>
         </div>
         <div className="flex flex-col space-y-8 p-4 justify-center">
           <AnimatedNumber value={270} />
-          <p className="text-black font-bold text-2xl">Completed Projects</p>
+          <p className="text-black font-bold text-2xl">{('completed-projects')}</p>
         </div>
       </div>
       </div>

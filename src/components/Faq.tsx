e58b17186e4a4e3858eae9536bf36455a15b8ff5
Fabrice <mwanafunzifabrice@gmail.com>
2023-09-05 16:25:26 +0200

    Pushing

--- src/components/Faq.tsx
+++ src/components/Faq.tsx
@@ -1,7 +1,6 @@
 import { Link } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import faqData from "../data/faq";
-import { AiOutlineDown } from "react-icons/ai";
 import vid2 from '../assets/vid2.mp4'
 
 

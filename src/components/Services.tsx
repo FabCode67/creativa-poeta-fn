c0014ea10845cff0eee9f7c057475a54ed3c601f
FabCode67 <mwanafunzifabrice@gmail.cpm>
2023-10-09 13:24:45 -0700

    pushing

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -1,10 +1,6 @@
-import { Link } from "react-router-dom";
-import { useTranslation } from "react-i18next";
 import blogData from "../data/blog";
 
-// About.tsx
 const Services = () => {
-  const { t } = useTranslation();
   console.log(blogData);
   
   return (
@@ -29,30 +25,7 @@ const Services = () => {
           <div className="icon w-[58%] h-fit min-h-32  justify-center my-auto flex items-center pl-3 laptop:text-base tablet:text-base desktop:text-base text-sm"><p className=" flex justify-center items-center  my-auto ">{blog.description}</p></div>
         </div>
         ))}
-       
-        {/* <div
-          className={`grid laptop:grid-cols-3 tablet:grid-cols-2 laptop:gap-16 grid-cols-1 mt-[3%] gap-4 sm:grid-cols-2`}
-        >
-          {blogData.map((blog) => (
-          <Link
-            className="border border-yellow-400 rounded-md px-4 py-2 h-[35rem]" to={`/services/${blog.id}`} key={blog.id}>
-            <img
-              src={blog.image}
-              alt="test"
-              className="w-full h-[55%] object-cover"
-            />
-            <h3 className="text-xl font-bold mt-2">
-            {blog.title} </h3>
-            <p className="mt-1 line-clamp-6">
-              {blog.description}
-            </p>
-            <button className="bg-[#EEBA2B] bottom-0 relative hover:bg-yellow-500 text-white w-full font-bold py-3 px-4 rounded mt-4">
-              {t('read_more')}
-            </button>
-          </Link>
-          ))}
-        </div>
-      */}
+      
       </div>
       </div>
       <div className="laptop:h-[6%] tablet:h-[4%] h-[2%] laptop:w-[10%] t w-[35%] absolute laptop:right-1/2 right-1/2 top-0 border-r-2 border-[#FFE533]">
--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom';
 import App from './App.tsx';
 import './index.css';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-import SingleServicePage from './components/SingleServicePage.tsx';
+// import SingleServicePage from './components/SingleServicePage.tsx';
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="/services/:id" element={<SingleServicePage />} />
+        {/* <Route path="/services/:id" element={<SingleServicePage />} /> */}
       </Routes>
     </Router>
   </React.StrictMode>,

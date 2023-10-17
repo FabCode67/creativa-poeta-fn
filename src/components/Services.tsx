a943550dd9eda75b19d6a3605154af2bd4e8b70f
FabCode67 <mwanafunzifabrice@gmail.com>
2023-10-17 11:35:05 +0200

    changes

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -10,15 +10,15 @@ const Services = () => {
         <h1 className="laptop:text-4xl text-2xl font-bold text-center text-[#EEBA2B]">
           NOS SERVICE
         </h1>
-        <div className="w-full  grid laptop:grid-cols-2 gap-20 desktop:grid-cols-2 tablet:grid-cols-1 grid-cols-1 laptop:p-28 desktop:p-28 tablet:p-8 p-4 ">
-        {/* {blogData.map((blog) => (
+        <div className="w-full  grid laptop:grid-cols-2 laptop:gap-20 desktop:gap-20 tablet:gap-12 gap-10 desktop:grid-cols-2 tablet:grid-cols-1 grid-cols-1 laptop:p-28 desktop:p-28 tablet:p-8 p-4 ">
+         {blogData.map((blog) => (
           <div className="flex  laptop:text-base desktop:text-base tablet:text-base text-sm p-1 mx-auto flex-col h-fit   laptop:w-[100%] desktop:w-[100%] tablet:w-[80%] w-[98%]">
             <div className="w-[100%] flex justify-self-start  h-[6rem] mt-0">
               <div className="flex object-contain icon w-[20%] my-auto justify-center">
                 <img src={blog.image} alt="" height={100} width={100} className="object-contain"></img>
               </div>
               <div className="flex flex-col icon w-fit p-3 laptop:h-[7rem] desktop:h-[7rem] tablet:h-[7rem] h-[6rem] space-y-2 self-end">
-                <div className="flex w-full text-lg desktop:text-3xl tablet:text-xl laptop:text-3xl text-white flex-col justify-end  self-end  content">  
+                <div className="flex w-full text-xl desktop:text-3xl tablet:text-xl laptop:text-3xl text-white flex-col justify-end  self-end  content">  
                   <p className="">{blog.h1}</p>
                   <p className="font-bold">{blog.h2}</p>
                 </div>
@@ -26,12 +26,12 @@ const Services = () => {
               </div>
             </div>
             <div className="w-full flex justify-self-start mt-0 p-1">
-              <p className="text-white pl-[22%] laptop:text-xl desktop:text-xl text-base"> {blog.description} </p>
+              <p className="text-white pl-[22%] laptop:text-xl desktop:text-xl tablet:text-xl text-sm"> {blog.description} </p>
             </div>
           </div>
-           ))} */}
+           ))} 
 
-         {blogData.map((blog) => (
+         {/* {blogData.map((blog) => (
         <div className="flex justify-center mx-auto  h-fit min-h-32 self-center laptop:w-[60%] desktop:w-[60%] tablet:w-[80%] w-[98%] text-white">
           <div className="icon w-[10%] h-8   my-auto flex flex-col justify-center text-center mt-15">
             <img src={blog.image} alt="line" className="o object-fill laptop:h-32 tablet:h-28 desktop:h-32 h-20 w-full  m-auto flex flex-col justify-center text-center"/>
@@ -43,11 +43,11 @@ const Services = () => {
           <div className="w-[2%] bg-[#EEBA2B]"></div> 
           <div className="icon w-[58%] h-fit min-h-32  justify-center my-auto flex items-center pl-3 laptop:text-base tablet:text-base desktop:text-base text-sm"><p className=" flex justify-center items-center  my-auto ">{blog.description}</p></div>
         </div>
-        ))} 
+        ))}  */}
       
       </div>
       </div>
-      <div className="laptop:h-[4%] desktop:[h-4%] tablet:h-[4%] h-[2%] laptop:w-[10%] t w-[35%] absolute laptop:right-1/2 right-1/2 top-0 border-r-2 border-[#FFE533]">
+      <div className="laptop:h-[4%] desktop:[h-4%] tablet:h-[3%] h-[2%] laptop:w-[10%] t w-[35%] absolute laptop:right-1/2 right-1/2 top-0 border-r-2 border-[#FFE533]">
       </div>
       <div className="very-buttom laptop:h-[4%] desktop:h-[4%] tablet:h-[4%] h-[2%] laptop:block desktop:block laptop:w-[5%] w-[2%] absolute laptop:left-2 left-2 bottom-0 border-r-2 border-[#FFE533]">
   <div className="buttons h-full flex cursor-pointer items-start space-x-1">

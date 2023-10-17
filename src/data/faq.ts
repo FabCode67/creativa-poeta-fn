735cfedec4231f0839b35a822f577c5e07777348
FabCode67 <mwanafunzifabrice@gmail.com>
2023-10-17 10:59:21 +0200

    changes

--- src/data/faq.ts
+++ src/data/faq.ts
@@ -16,7 +16,7 @@ const faqData = [
     {
         id: 3,
         title: 'TRANSFORMEZ VOS IDEES EN RÉALITÉ',
-        description: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et'
+        description: 'Sed ut derspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et'
     },
     {
         id: 4,

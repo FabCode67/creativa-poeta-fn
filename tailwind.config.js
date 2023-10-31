b583eba612e407d83a7272fd82d3ef5e270740c7
FabCode67 <mwanafunzifabrice@gmail.com>
2023-10-31 18:53:56 +0200

    fixixng:

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,7 @@ module.exports = {
       phone: "360px",
       tablet: "640px",
       ipod: "768px",
+      sumsung: "412px",
       iphone: "414px",
       laptop: "1024px",
       desktop: "1280px",

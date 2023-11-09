acf6ef40d07e8514546539ff17665ec26e1714e8
FabCode67 <mwanafunzifabrice@gmail.com>
2023-11-09 16:44:18 +0200

    wrap things

--- src/components/MainFooter.tsx
+++ src/components/MainFooter.tsx
@@ -2,7 +2,7 @@
 const MainFooter = () => {
   return (
     <div className='flex flex-wrap mx-auto text-center items-center justify-center w-full gap-y-2 laptop:justify-between desktop:justify-between desktop:flex-row laptop:flex-row flex-col-reverse bg-black h-fit p-2 py-4'>
-    <div className='flex flex-row justify-center items-center space-x-2 mr-2'>
+    <div className='flex flex-row flex-wrap justify-center items-center space-x-2 mr-2'>
     <p className='text-xs font-bold text-slate-400'>Mentions légale</p>
     <p className='text-xs font-bold text-slate-400'>|</p>
     <p className='text-xs font-bold text-slate-400'>Conditions générales</p>

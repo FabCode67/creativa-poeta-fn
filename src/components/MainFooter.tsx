dcf0d807049caef0c3c05012aebdd271916d03c1
FabCode67 <mwanafunzifabrice@gmail.com>
2023-11-09 16:29:30 +0200

    wrap things

--- src/components/MainFooter.tsx
+++ src/components/MainFooter.tsx
@@ -1,14 +1,15 @@
 
 const MainFooter = () => {
   return (
-    <div className='flex flex-wrap mx-auto text-center items-center justify-center w-full laptop:justify-between desktop:justify-between bg-black h-fit p-2 py-4'>
+    <div className='flex flex-wrap mx-auto text-center items-center justify-center w-full gap-y-2 laptop:justify-between desktop:justify-between desktop:flex-row laptop:flex-row flex-col-reverse bg-black h-fit p-2 py-4'>
     <div className='flex flex-row justify-center items-center space-x-2 mr-2'>
     <p className='text-xs font-bold text-slate-400'>Mentions légale</p>
     <p className='text-xs font-bold text-slate-400'>|</p>
     <p className='text-xs font-bold text-slate-400'>Conditions générales</p>
+    <p className='text-xs font-bold text-slate-400'>|</p>
+    <p className='text-xs font-bold text-slate-400'>Politique de cookies et de confidentialité</p>
     </div>
-    <p className='text-xs font-bold text-slate-400  ml-1'> © 2023 Creativa Poeta. Tous droits réservés. </p>
-    <p className='text-xs font-bold text-slate-400 laptop:mt-0 desktop:mt-0 mt-2 ml-2'>Politique de cookies et de confidentialité</p>
+    <p className='text-xs font-bold text-slate-400 laptop:mt-0 desktop:mt-0'> © 2023 Creativa Poeta. Tous droits réservés. </p>
   </div>
   )
 }

49259653c3a79702765f3a3aecc06fd3ea3f5c15
FabCode67 <mwanafunzifabrice@gmail.com>
2023-11-10 13:17:26 +0200

    fixing

--- src/components/MainFooter.tsx
+++ src/components/MainFooter.tsx
@@ -1,7 +1,7 @@
 
 const MainFooter = () => {
   return (
-    <div className='flex flex-wrap mx-auto text-center items-center justify-center w-full gap-y-2 laptop:justify-between desktop:justify-between desktop:flex-row laptop:flex-row flex-col-reverse bg-black h-fit p-2 py-4'>
+    <div className='flex flex-wrap mx-auto text-center items-center justify-center w-full gap-y-2 laptop:justify-between desktop:justify-between desktop:flex-row px-8 laptop:flex-row flex-col-reverse bg-black h-fit p-2 py-4'>
     <div className='flex flex-row flex-wrap justify-center items-center space-x-2 mr-2'>
     <p className='text-xs font-bold text-slate-400'>Mentions légales</p>
     <p className='text-xs font-bold text-slate-400'>Conditions générales</p>

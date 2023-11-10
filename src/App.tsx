acc43f6da99796c249752242591b343a7e9bbbd6
FabCode67 <mwanafunzifabrice@gmail.com>
2023-11-10 21:05:20 +0200

    nos projects

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ function  App() {
         <About />
         <Services />
         <Projects />
-        <Parteners />
+        {/* <Parteners /> */}
         <Faq />
         {/* <Contact /> */}
         <Footer />
